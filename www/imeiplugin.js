32680111a0c7d30752577787eacb57513252d3af
lvdou <wangxvjing@21cn.com>
2016-07-06 17:41:47 +0800

    Update imeiplugin.js

--- www/imeiplugin.js
+++ www/imeiplugin.js
@@ -1,11 +1,11 @@
-function IMEIPlugin() {
-	this.getImei = function(returnCallback) {
-		cordova.exec(returnCallback, function(){}, "imeiplugin", "getImei", []);
+var cordova = require('cordova');
+var androidIMEI = {
+    	
+  function IMEIPlugin() {
+	this.getImei = function(successCallback, errorCallback) {
+		cordova.exec(successCallback, errorCallback, "androidIMEI", "getImei", []);
 	}
+  }
 }
 
-if(!window.plugins) {
-	window.plugins = {}
-}
-
-window.plugins.imeiplugin = new IMEIPlugin();
+module.exports = androidIMEI;
